84ef39430b2d3cf613636fdba951495c7e618644
gregdolley <gregdolley@gmail.com>
2023-07-30 11:36:58 -0700

    WIP (see details) -Added Linux and MacOS support (not tested yet) -Removed anti-aliasing on text to image conversion. -Added command-line options: output file name override, font override, and image width override. -Massive refacoring from previous version (smaller functions, more logical control flow, better function names, much more readable code). -Changed default font and added fallback support. -Improved exception handling

--- ascii-art.py
+++ ascii-art.py
@@ -8,49 +8,103 @@
 # ASCII art, but it can do it while being constrained to only using 11 basic ASCII 
 # characters.
 #
-# TODO: this code is messy and I think it can be refactored into much neater and simpler code.
-# Also, remove this file from the test-python project and put it in its own project.
+# TODO: remove this file from the test-python project and put it in its own project.
 #---------------------------------------------------------------------------------------------
+import argparse
+import os
+import platform
+import sys
+import traceback
 from math import ceil
 
 from PIL import Image, ImageDraw, ImageFont
 
 ASCII_CHARS = ["@", "#", "$", "%", "?", "*", "+", ";", ":", ",", "."]
+DEFAULT_OUTPUT_FILENAME = "ascii_image"
+
+user_font_file = ""
 
 def main():
-    try:
-        image, new_image_width = get_user_config()
-        image = resize(image, int(new_image_width, base=10) if new_image_width.isnumeric() else image.width)
+    image, new_image_width, output_filename = get_user_config()
+    image = resize(image, new_image_width)
+    print("Generating ASCII art text string...")
+    ascii_img_text = generate_ascii_art(image)
 
-        greyscale_image = image.convert("L") # convert image to greyscale
-        ascii_str = pixels_to_ascii_chars(greyscale_image)
-        img_width = greyscale_image.width
-        ascii_img=""
+    output_text_filename, output_image_filename = get_output_filenames_with_ext(output_filename)
+    print("Writing text string to file: {0}".format(output_text_filename))
+    with open(output_text_filename, "w") as f: f.write(ascii_img_text) # save the string to a file to check for accuracy        
+    print("Done.");
+    
+    print("Creating image version of ASCII text file...")
+    textfile_to_image(output_text_filename).save(output_image_filename) # convert the text file to an image as if it were a screenshot
+    print("Done.")
 
-        # split the string based on width  of the image
-        for i in range(0, len(ascii_str), img_width):
-            ascii_img += ascii_str[i:i+img_width] + "\n"
+    print(f"ASCII art text file generated: {output_text_filename}")
+    print(f"Image version of the same file: {output_image_filename}")
 
-        # save the string to a file to check for accuracy
-        with open("ascii_image.txt", "w") as f: f.write(ascii_img)
+    return 0
 
-        # convert the text file to an image as if it were a screenshot
-        textfile_to_image("ascii_image.txt").save("ascii_image.png")
-    except:
-        pass
+
+def file_exists(filename): return (os.path.isfile(filename) == True)
+def convert_image_to_grayscale(image): return image.convert("L")
+def disable_antialiasing(renderer): renderer.fontmode = "1" # turns off anti-aliasing on TT fonts (renderer = ImageDraw object)
+
+
+def generate_ascii_art(input_image):
+    greyscale_image = convert_image_to_grayscale(input_image)
+    ascii_str = pixels_to_ascii_chars(greyscale_image)
+    img_width = greyscale_image.width
+    ascii_img_text = ""
+
+    # add line breaks
+    for i in range(0, len(ascii_str), img_width):
+        ascii_img_text += ascii_str[i:i+img_width] + "\n"
+    
+    return ascii_img_text
+
+
+def get_output_filenames_with_ext(output_filename):
+    fname_data = dict(filename=output_filename, txt_ext="txt", img_ext="png");
+    output_text_filename = "{filename}.{txt_ext}".format(**fname_data)
+    output_image_filename = "{filename}.{img_ext}".format(**fname_data)
+    return output_text_filename, output_image_filename
+
+    
+def draw_text_strings_as_graphical_lines(lines, renderer, line_height, font):
+    # draw each line of text onto image
+    for i, line in enumerate(lines):
+        renderer.text((0, int(round(i * line_height))), line, fill=0, font=font)
+
+
+def prompt_for_image_file():
+    path = ""
+
+    while(len(path) == 0):
+        path = input("Enter the path to the image file: ").strip()
+        if(len(path) == 0): print("Received blank input.")
+
+    return path
 
 
 def get_user_config():
-    path = input("Enter the path to the image file: ")
-    
-    try:
-        image = Image.open(path)
-    except:
-        print("Unable to open that image file. Please check your path and try again.")
-        raise
+    parser = argparse.ArgumentParser(description='Command-line example.')
+    parser.add_argument('-o', action='store', dest='output',
+                        metavar='FILE',
+                        help='Use FILE as output file name (without extension) instead of default (ascii_image.txt/.png)')
+    parser.add_argument('-f', action='store', dest='font_file',
+                        metavar='FONT_FILE_PATH',
+                        help='Use font file FONT_FILE_PATH for ASCII art font instead of default')
+    parser.add_argument('-w', action='store', dest='resize_image_width',
+                        metavar='RESIZE_IMAGE_PIXEL_WIDTH',
+                        help='Resize the input image width to RESIZE_IMAGE_PIXEL_WIDTH pixels before converting to ASCII art (allows you to shrink/grow the input image if it is too big/small)')
 
-    new_image_width = input("Resize input image to the following width (leave blank to skip): ")
-    return image, new_image_width
+    args = parser.parse_args()
+    path = prompt_for_image_file()
+    image = Image.open(path)
+    new_image_width = int(args.resize_image_width, base=10) if "resize_image_width" in args and args.resize_image_width != None else image.width
+    output_filename = args.output if "output" in args and args.output != None else DEFAULT_OUTPUT_FILENAME
+
+    return image, new_image_width, output_filename
 
 
 def pixels_to_ascii_chars(image):
@@ -58,23 +112,57 @@ def pixels_to_ascii_chars(image):
     ascii_str = ""
 
     for pixel in pixels:
+        # TODO: it would be better if this was a non-linear scale based on the grayscale histogram of the image
         ascii_str += ASCII_CHARS[pixel//25]
     return ascii_str
 
 
 def resize(image, new_width):
-    if new_width == image.width: return image
+    if new_width == None: return image
+    if new_width <= 0: return image
+    if new_width == image.width: return image # no resize needed
     
     new_height = new_width * image.height / image.width
     return image.resize((new_width.__round__(), new_height.__round__()))
 
 
-def textfile_to_image(textfile_path):
-    # remove any trailing white spaces from each line
+def get_monospace_font_filename():
+    operating_system = platform.system()
+
+    if operating_system == "Windows":
+        possible_fonts = ["c:/windows/fonts/consola.ttf"]
+    elif operating_system == "Linux":
+        possible_fonts = ["/usr/share/fonts/truetype/freefont/FreeMono.ttf",
+                          "/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf"]
+    elif operating_system == "Darwin": # MacOS
+        possible_fonts = ["/System/Library/Fonts/Monaco.ttf"]
+
+    for font_file in possible_fonts:
+        if file_exists(font_file) == True:
+            return font_file
+
+    raise FileNotFoundError("Can't find a suitable font file for your operating system.")
+
+
+def create_grayscale_image_with_renderer(image_width, image_height):
+    # Remember that grayscale images don't have RGB pixel values - they have
+    # a single color channel going from 0 (black) to 255 (white) and all shades
+    # of gray in between (from darkest gray of 1 to lightest gray of 254).
+    # In Image.new() below, "L" is for a 256-color grayscale palette and 
+    # "color=255" specifies the default background color should be white.
+    image = Image.new("L", (image_width, image_height), color=255)
+    return image, ImageDraw.Draw(image)
+
+
+def read_all_lines_rstrip(textfile_path):
     with open(textfile_path) as f:
         lines = tuple(line.rstrip() for line in f.readlines())
+    return lines
 
-    font_filename = "c:/windows/fonts/consola.ttf"
+
+def textfile_to_image(textfile_path):
+    lines = read_all_lines_rstrip(textfile_path) # read all text lines and put into "lines" array (also rstrip any trailing whitespaces)
+    font_filename = get_monospace_font_filename()
 
     try:
         font = ImageFont.truetype(font_filename, size=12)
@@ -82,32 +170,51 @@ def textfile_to_image(textfile_path):
         print(f'Could not load font "{font_filename}".')
         raise
 
-    # find tallest line out of the entire set; that height will be used 
-    # on _all_ the lines that are rendered to the image file
-    tallest_line = max(lines, key=lambda line: font.getbbox(line)[3])
-    max_line_height = font.getbbox(tallest_line)[3]+font.getmetrics()[1]
-    image_height = int(ceil(max_line_height * len(lines)))
+    image_height, max_line_height = sum_pixel_height_for_all_lines(lines, font)
+    image_width = calc_widest_line_in_pixels(lines, font)
+    new_image, renderer = create_grayscale_image_with_renderer(image_width, image_height)
+    disable_antialiasing(renderer)
+    draw_text_strings_as_graphical_lines(lines, renderer, max_line_height, font)
 
-    # similar to above, find the line that will require the most number
-    # of horizontsl pixels
+    return new_image
+    
+
+def calc_widest_line_in_pixels(lines, font):
     widest_line = max(lines, key=lambda s: font.getbbox(s)[2])
     max_line_width = font.getbbox(widest_line)[2]
-    image_width = int(ceil(max_line_width))
+    return int(ceil(max_line_width))
 
-    # create image and drawing objects (default background color of 
-    # image is specified by the "color" parameter)
-    image = Image.new("L", (image_width, image_height), color=255) # "L" for 8-bit greyscale palette
-    draw = ImageDraw.Draw(image)
-    draw.fontmode = "1" # turns off anti-aliasing on TT fonts
 
-    # draw each line of text onto image
-    horizontal_position = 0
-    for i, line in enumerate(lines):
-        vertical_position = int(round(i * max_line_height))
-        draw.text((horizontal_position, vertical_position), line, fill=0, font=font)
+def calc_tallest_line_in_pixels(lines, font):
+    tallest_line = max(lines, key=lambda line: font.getbbox(line)[3])
+    return font.getbbox(tallest_line)[3]+font.getmetrics()[1]
 
-    return image
+
+def sum_pixel_height_for_all_lines(lines, font):
+    max_line_height = calc_tallest_line_in_pixels(lines, font)
+    return int(ceil(max_line_height * len(lines))), max_line_height
+
+
+def callstack_prompt(from_traceback):
+    while True:
+        resp = input("Show callstack? (y/n) ").strip()
+        if(len(resp) == 0 or resp == "n"): break
+        elif(resp == "y"): 
+            traceback.print_tb(from_traceback) 
+            break
+        
+        print("Invalid input. Please try again (note: response is case-sensitive).")
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        status = main()
+
+        print("Done.")
+        print(f"Exit status code:  {status}")
+        
+        sys.exit(status)
+    except Exception as e:
+        print(sys.exc_info()[1])
+        callstack_prompt(sys.exc_info()[2])
+        sys.exit(-1)
